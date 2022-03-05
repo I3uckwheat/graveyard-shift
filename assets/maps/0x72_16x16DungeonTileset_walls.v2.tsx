6b7f293125b36f16482bc55b2145c845aa3ea227
Briggs Elsperger <briggse@protonmail.com>
2022-03-04 21:56:02 -0700

    initial commit

---
+++ assets/maps/0x72_16x16DungeonTileset_walls.v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="0x72_16x16DungeonTileset_walls.v2" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../0x72_16x16DungeonTileset_walls.v2.png" width="256" height="256"/>
+</tileset>
